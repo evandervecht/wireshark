e4a0f685b7d2e87bc980cea0012c6473a8928ef2
Ellert van der Vecht <evandervecht@schubergphilis.com>
2015-12-10 17:44:42 +0100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Ellert van der Vecht'
 maintainer_email 'evandervecht@schubergphilis.com'
 license 'All rights reserved'
 description 'Installs WinPcap and Wireshark'
-version '1.0.0'
+version '1.0.1'
 recipe 'sbp_wireshark::default', 'WinPcap and Wireshark install'
 
 supports 'windows'

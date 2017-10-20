2969a58e6fc3e4cee27ef7f310067394ecc1431a
sahed rahman salam <salamsahed@gmail.com>
2017-10-20 07:36:36 +0100

    updated the support version.

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,10 @@ maintainer 'Ellert van der Vecht'
 maintainer_email 'evandervecht@schubergphilis.com'
 license 'All rights reserved'
 description 'Installs WinPcap and Wireshark'
-version '1.0.1'
+version '2.0.0'
 recipe 'sbp_wireshark::default', 'WinPcap and Wireshark install'
 
 supports 'windows'
+supports 'ubuntu', '>= 16.04'
 
 depends 'windows'
